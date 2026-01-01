768f470218f8a33eb0949e40022facaaefa1d999
Siouar JLidi <siouar.jlidi@isimg.tn>
2026-01-01 20:29:46 +0100

    Update temperature_control.ino.ino

--- temperature_control.ino.ino
+++ temperature_control.ino.ino
@@ -1,7 +1,6 @@
 // ---------- Déclaration des pins ----------
 int sensorPin = A0;    // LM35 connecté à A0
 int ledPin = 13;       // LED connectée à D13
-int buzzerPin = 12;    // Buzzer connecté à D12 (optionnel)
 
 // ---------- Variables ----------
 float temperature;     
@@ -27,10 +26,8 @@ void loop() {
   // Condition : si température > 30°C
   if (temperature > 30) {
     digitalWrite(ledPin, HIGH);    // Allume LED
-    digitalWrite(buzzerPin, HIGH); // Active buzzer
   } else {
     digitalWrite(ledPin, LOW);     // Éteint LED
-    digitalWrite(buzzerPin, LOW);  // Éteint buzzer
   }
 
   delay(1000); // Actualiser chaque seconde
